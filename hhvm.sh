34cb4c5615499b7b4173996bfbd7319d926f7b76
Jascha <jascha@jaeh.at>
2014-03-11 17:16:45 +0100

    Updated directory targets in hhvm.sh, added hhvm.pid to .gitignore

--- hhvm.sh
+++ hhvm.sh
@@ -3,23 +3,23 @@
 case "$1" in
   start)
 		echo "Starting hhvm"
-        hhvm -m daemon -p 80 -c /var/www/hhvm.hdf
+        hhvm -m daemon -p 80 -c /docker/config/hhvm.hdf
 		echo "[Ok]"
         ;;
   stop)
-		echo "Killing hhvm with pid `cat /var/www/hhvm.pid`"
-        kill -SIGTERM `cat /var/www/hhvm.pid`
+		echo "Killing hhvm with pid `cat /docker/config/hhvm.pid`"
+        kill -SIGTERM `cat /docker/config/hhvm.pid`
 		echo "[OK]"
         ;;
   restart)
-		echo "Killing hhvm with pid `cat /var/www/hhvm.pid`"
-        kill -SIGTERM `cat /var/www/hhvm.pid`
+		echo "Killing hhvm with pid `cat /docker/config/hhvm.pid`"
+        kill -SIGTERM `cat /docker/config/hhvm.pid`
         echo "Starting hhvm"
-        hhvm -m daemon -p 80 -c /var/www/hhvm.hdf
+        hhvm -m daemon -p 80 -c /docker/config/hhvm.hdf
 		echo "[OK]"
         ;;
   *)
-        echo "Usage: /var/www/hhvm.sh {start|stop|restart}"
+        echo "Usage: hhvmd {start|stop|restart}"
         exit 1
 esac
 
